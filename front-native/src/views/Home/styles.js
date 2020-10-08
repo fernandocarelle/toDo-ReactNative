4c5fc756971de869170ac16f345e024ee4dd6d0b
fernandocarelle <fern@ndo.codes>
2020-10-08 13:34:03 -0300

    criacao dos filtros

--- front-native/src/views/Home/styles.js
+++ front-native/src/views/Home/styles.js
@@ -7,6 +7,24 @@ const styles = StyleSheet.create({
         backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'flex-start'
+    },
+    filter: {
+        flexDirection: 'row',
+        width: '100%',
+        justifyContent: 'space-around',
+        height: 70,
+        alignItems: 'center'
+    },
+    filterTextActived: {
+        fontWeight: 'bold',
+        fontSize: 18,
+        color: '#F4A261'
+    },
+    filterTextInative: {
+        fontWeight: 'bold',
+        fontSize: 18,
+        color: '#2A9D8F',
+        opacity: 0.5
     }
 
 });
